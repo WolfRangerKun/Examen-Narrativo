40ab917a6e27df17d4681a04a9a6fe0ab5e77782
Gaboxz <80337191+Gaboxz@users.noreply.github.com>
2021-11-10 09:21:16 -0300

    aaaaaaa

--- Assets/_Scripts/Gabo/RayObservation.cs
+++ Assets/_Scripts/Gabo/RayObservation.cs
@@ -8,36 +8,42 @@ public class RayObservation : MonoBehaviour
     PlayerMovementIsaac player;
     public Transform directionRay;
     public LayerMask layerRay;
+    public float distanceRay;
     public void Start()
     {
         player = FindObjectOfType<PlayerMovementIsaac>();
     }
     public void Update()
     {
-        //Vector3 mouse = Input.mousePosition;
         if (player.changeCamera && Input.GetKey(KeyCode.E))
         {
+
             RaycastHit hit;
-            Debug.DrawRay(transform.position, directionRay.position, Color.green, 10);
-            Physics.Raycast(transform.position, directionRay.position, out hit, 10, layerRay);
-            if (hit.collider)
+            Debug.DrawRay(transform.position, transform.forward, Color.blue, 10);
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 10, layerRay))
             {
                 Debug.Log("wena ctm");
-                hit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+                hit.transform.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
             }
+            
         }
- 
     }
 
-    void ObservationDetected()
-    {
-        RaycastHit hit;
-        Debug.DrawRay(transform.position, directionRay.position, Color.green, 10);
-        Physics.Raycast(transform.position, directionRay.position, out hit, 10, layerRay);
-        if (hit.collider)
-        {
-            Debug.Log("wena ctm");
-            hit.collider.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
-        }
-    }
+    //public void ObservationDetected()
+    //{
+    //    RaycastHit hit;
+    //    if (Physics.Raycast(transform.position, directionRay.position, out hit, layerRay))
+    //    {
+    //        Debug.Log("wena ctm");
+    //        hit.transform.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+    //    }
+    //    Debug.DrawRay(transform.position, directionRay.position, Color.blue);
+
+    //}
+
+    //public void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.green;
+    //    Gizmos.DrawRay(transform.position, transform.forward);
+    //}
 }
