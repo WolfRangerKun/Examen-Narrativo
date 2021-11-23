7f5ccb5ba4fa48438be64e11bc7fa8d197bf4bbc
Gaboxz <80337191+Gaboxz@users.noreply.github.com>
2021-11-22 21:58:45 -0300

    tu mama en tanga

--- Assets/_Scripts/Gabo/RayObservation.cs
+++ Assets/_Scripts/Gabo/RayObservation.cs
@@ -10,6 +10,7 @@ public class RayObservation : MonoBehaviour
     public LayerMask layerRay;
     public float distanceRay;
     public Transform target;
+    public Vector3 targetVector;
 
     public void Start()
     {
@@ -17,11 +18,12 @@ public class RayObservation : MonoBehaviour
     }
     public void Update()
     {
+        //cameraSwitch = FindObjectOfType<CinemachineVirtualCamera>().Priority = h
         if (player.changeCamera && Input.GetKey(KeyCode.E))
         {
             RaycastHit hit;
-            Debug.DrawRay(transform.position, target.transform.position, Color.blue, distanceRay);
-            if (Physics.Raycast(transform.position, target.transform.position, out hit, distanceRay, layerRay))
+            Debug.DrawRay(/*cameraSwitch.position*/ transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.blue, distanceRay);
+            if (Physics.Raycast(/*cameraSwitch.position*/transform.position, transform.TransformDirection(Vector3.forward) * 1000, out hit, distanceRay, layerRay))
             {
                 hit.transform.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
                 Libreta.instance.RegisterGeturess(hit.transform.gameObject.GetComponent<ObservGests>().descripcion, hit.transform.gameObject.GetComponent<ObservGests>().gestoObservable);
